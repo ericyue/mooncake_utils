da96532088a4dff5c877566efc2591ef68dcfad7
ericyue <hi.moonlight@gmail.com>
2017-06-07 17:22:23 +0800

    up

---
+++ alert.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+#encoding: utf-8
+import os,sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+base = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(base)
+from slacker import Slacker
+
+
+slack = Slacker(token)
+
+def alert(msg, mail = True, slack = True, channel="#mooncake"):
+    print msg
+    try:
+      send_slack(msg, channel)
+    except Exception, w:
+      print w
+      pass
+
+def send_slack(msg, channel="#mooncake"):
+  attempt = 0
+  while attempt < 3:
+    try:
+      slack.chat.post_message(channel, msg)
+      break
+    except Exception,w:
+      print "retry#%s send slack due to [%s]" %(attempt,w)
+      attempt+=1
+
+if __name__ == "__main__":
+  alert("im alert")
