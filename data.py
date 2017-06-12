c655b297ff2445461a4cfaf21efdd85780b83aeb
ericyue <hi.moonlight@gmail.com>
2017-06-12 15:59:06 +0800

    up

---
+++ data.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+
+# @author Yue Bin ( hi.moonlight@gmail.com )
+# @date 2017-06-07
+import os,sys
+import mmap
+
+def read_mmap(fn):
+  with open(fn, "r+b") as f:
+    map = mmap.mmap(f.fileno(), 0, prot=mmap.PROT_READ)
+    for line in iter(map.readline, ""):
+      yield line.rstrip('\n')
+
+if __name__ == "__main__":
+  for i in read_mmap('/data2/yuebin/sixroom/zhubo2vec/output/follow_full_20170519.log'):
+      print i
+      break
