84c171356993a74c6619f09f6075495b7eadccc8
ericyue <hi.moonlight@gmail.com>
2017-06-13 14:20:24 +0800

    up

--- file.py
+++ file.py
@@ -3,7 +3,14 @@
 # @author Yue Bin ( hi.moonlight@gmail.com )
 # @date 2017-06-07
 import os
+import glob
 
 def mkdirp(directory):
   if not os.path.isdir(directory):
     os.makedirs(directory)
+
+def rm_folder(path):
+  files = glob.glob(path)
+  for one in files:
+    print "removing [%s]", one
+    os.remove(one)
