b28c07dfe1c27a65c846a8ffca18dec4dd717dba
ericyue <hi.moonlight@gmail.com>
2017-06-15 13:13:06 +0800

    up

--- cmd.py
+++ cmd.py
@@ -6,8 +6,9 @@ def run_cmd(cmd):
   exit_code = status >> 8
   signal_num = status % 256
   print ' ==> Exit: %d, Signal: %d, Core?: %s ' % (exit_code, signal_num, bool(exit_code / 256))
-  print ' ==> Output: %s' % text
+  print ' ==> Output:\n%s' % text
   print '--------- Command END ---------'
+  print ''
   return status, text
 
 if __name__ == "__main__":
