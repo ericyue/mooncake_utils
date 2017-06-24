4e5a66a9dab67d0e0382e43b2d821e75af2bc9e2
ericyue <hi.moonlight@gmail.com>
2017-06-24 13:28:56 +0800

    up

---
+++ setup.py
@@ -0,0 +1,20 @@
+# -*- coding:utf-8 -*-
+#
+# Copyright (c) 2017 mooncake. All Rights Reserved
+####
+# @brief
+# @author Eric Yue ( hi.moonlight@gmail.com )
+# @version 0.0.1
+from distutils.core import setup
+setup(
+  name = 'mooncake_utils',
+  packages = ['mooncake_utils'], # this must be the same as the name above
+  version = '0.1',
+  description = 'just a useful utils for mooncake personal project.',
+  author = 'mooncake',
+  author_email = 'hi.moonlight@gmail.com',
+  url = 'https://github.com/ericyue/mooncake_utils', 
+  download_url = 'https://github.com/ericyue/mooncake_utils/archive/0.1.zip', # I'll explain this in a second
+  keywords = ['utils','data','machine-learning'], # arbitrary keywords
+  classifiers = [],
+)
