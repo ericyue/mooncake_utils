0a44004fb0fe4d24be10a9181f8675dfbecc3d1f
ericyue <hi.moonlight@gmail.com>
2017-06-24 12:59:16 +0800

    up

--- alert.py
+++ alert.py
@@ -3,19 +3,46 @@
 import os,sys
 reload(sys)
 sys.setdefaultencoding("utf-8")
-base = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+base = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(base)
 from slacker import Slacker
 import ConfigParser
   
 conf = ConfigParser.ConfigParser({})
-conf.read("./conf/alert.conf")
+conf.read("%s/conf/alert.conf" % base)
 channel = conf.get("slack","channel")
 token = conf.get("slack","token")
 
 slack = Slacker(token)
 
 def alert(msg, mail = True, slack = True, channel="#mooncake"):
+    """This function does something.
+
+    Args:
+       name (str):  The name to use.
+
+    Kwargs:
+       state (bool): Current state to be in.
+
+    Returns:
+       int.  The return code::
+
+          0 -- Success!
+          1 -- No good.
+          2 -- Try again.
+
+    Raises:
+       AttributeError, KeyError
+
+    A really great idea.  A way you might use me is
+
+    >>> print public_fn_with_googley_docstring(name='foo', state=None)
+    0
+
+    BTW, this always returns 0.  **NEVER** use with :class:`MyPublicClass`.
+
+    """
+
     print msg
     try:
       send_slack(msg, channel)
@@ -24,6 +51,16 @@ def alert(msg, mail = True, slack = True, channel="#mooncake"):
       pass
 
 def send_slack(msg, channel="#mooncake"):
+  """This function does something.
+
+  :param name: The name to use. 
+  :type name: str. 
+  :param state: Current state to be in. 
+  :type state: bool. 
+  :returns: int -- the return code. 
+  :raises: AttributeError, KeyError
+
+  """ 
   attempt = 0
   while attempt < 3:
     try:
--- docs/index.rst
+++ docs/index.rst
@@ -9,7 +9,7 @@ Welcome to mooncake utils's documentation!
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
-                modules.rst
+     mooncake_utils
 
 
 
