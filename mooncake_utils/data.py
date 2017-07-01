c1b73e3da8a9a2994a1d32c9f477da143ab7de01
ericyue <hi.moonlight@gmail.com>
2017-07-01 13:49:33 +0800

    up

--- mooncake_utils/data.py
+++ mooncake_utils/data.py
@@ -5,6 +5,56 @@
 import os,sys
 import mmap
 
+def _strips(direction, text, remove):
+  if isinstance(remove, iters):
+    for subr in remove:
+      text = _strips(direction, text, subr)
+    return text
+  
+  if direction == 'l': 
+    if text.startswith(remove): 
+      return text[len(remove):]
+  elif direction == 'r':
+    if text.endswith(remove):   
+      return text[:-len(remove)]
+  else: 
+    raise ValueError("Direction needs to be r or l.")
+  return text
+
+def rstrips(text, remove):
+  """
+  removes the string `remove` from the right of `text`
+    >>> rstrips("foobar", "bar")
+    'foo'
+  
+  """
+  return _strips('r', text, remove)
+
+def lstrips(text, remove):
+  """
+  removes the string `remove` from the left of `text`
+  
+    >>> lstrips("foobar", "foo")
+    'bar'
+    >>> lstrips('http://foo.org/', ['http://', 'https://'])
+    'foo.org/'
+    >>> lstrips('FOOBARBAZ', ['FOO', 'BAR'])
+    'BAZ'
+    >>> lstrips('FOOBARBAZ', ['BAR', 'FOO'])
+    'BARBAZ'
+  
+  """
+  return _strips('l', text, remove)
+
+def strips(text, remove):
+  """
+  removes the string `remove` from the both sides of `text`
+    >>> strips("foobarfoo", "foo")
+    'bar'
+  
+  """
+  return rstrips(lstrips(text, remove), remove)
+
 def dictfind(dictionary, element):
   """
   Returns a key whose value in `dictionary` is `element` 
