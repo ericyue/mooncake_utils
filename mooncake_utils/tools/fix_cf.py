c8f48f26e8565b701a0a123770081585c80142fc
ericyue <hi.moonlight@gmail.com>
2017-07-04 00:55:30 +0800

    up

--- mooncake_utils/tools/fix_cf.py
+++ mooncake_utils/tools/fix_cf.py
@@ -4,37 +4,37 @@ import glob
 from mooncake_utils.date import *
 
 if __name__ == "__main__":
-  output_fn = "./%s.dat.fix" % gen_today(delta=0, with_time=True)
-  output = open(output_fn, "w")
   input_fn = sys.argv[1]
+  output_fn = "%s.fix" % input_fn
   print "input[%s] output[%s]" % (input_fn, output_fn)
 
-  files = glob.glob(input_fn)
+  output = open(output_fn, "w")
+
   bad = 0
-  for filename in files:
-    print filename
-    with open(filename, "r+b") as f:
-        try:
-          map = mmap.mmap(f.fileno(), 0, prot=mmap.PROT_READ)
-        except Exception,w:
-          print w
-          continue
+  with open(input_fn, "r+b") as f:
+      try:
+        map = mmap.mmap(f.fileno(), 0, prot=mmap.PROT_READ)
+      except Exception,w:
+        print w
+        exit()
 
-        for line in iter(map.readline, ""):
-          if line[-1] == "\n":
-            key,value = line.strip().split('\x01')
-            new_value = ""
-            v_items = value.split(',')
-            for v in v_items:
-              try:
-                int(v.split(':')[0])
-                new_value += "%s," % v
-              except:
-                continue
+      for line in iter(map.readline, ""):
+        if line[-1] == "\n":
+          key,value = line.strip().split('\x01')
+          new_value = ""
+          v_items = value.split(',')
+          for v in v_items:
+            try:
+              int(v.split(':')[0])
+              new_value += "%s," % v
+            except Exception,w:
+              print w,line
+              continue
 
-            output.write("%s\x01%s\n" % (key,new_value.rstrip(',')))
-          else:
-            bad +=1
-            print "badline #%s" % bad
+          output.write("%s\x01%s\n" % (key,new_value.rstrip(',')))
+        else:
+          bad +=1
+          print "badline #%s" % bad
   
+  print "total bad line:", bad
   output.close()
