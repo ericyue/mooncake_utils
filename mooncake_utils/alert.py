3322b6fa825a85e6d7c6e0af98c3bf543facaa95
ericyue <hi.moonlight@gmail.com>
2017-07-05 13:50:32 +0800

    up

--- mooncake_utils/alert.py
+++ mooncake_utils/alert.py
@@ -4,59 +4,86 @@ reload(sys)
 sys.setdefaultencoding("utf-8")
 base = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(base)
+from mooncake_utils.log import *
 from slacker import Slacker
-import ConfigParser
-  
-conf = ConfigParser.ConfigParser({})
-conf.read("%s/conf/alert.conf" % base)
-channel = conf.get("slack","channel")
-token = conf.get("slack","token")
-slack = Slacker(token)
+import yaml
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
 
+default_conf_path = os.getenv('MOONCAKE_UTILS_ALERT_CONF', base)
+  
 """
 alert module
 """
 
-def alert(msg, mail = True, slack = True, channel="#mooncake"):
-    """
-    用来发送报警,支持邮件和Slack
+logger = get_logger(debug=True, name='mu.alert', with_file=False)
 
-    :param msg: 报警内容
-    :param mail: 是否开启邮件报警
-    :param slack: 是否开启slack报警
-    :param channel: slack报警的接收频道
+class Alert:
 
-    """
+  conf_path = default_conf_path
+  slack = None
 
-    print msg
+  enable_slack = True
+  enable_mail = True
+
+  default_channel = None
+  
+  def __init__(self, conf_path = None):
+    if conf_path:
+      self.conf_path = conf_path
+
+    logger.info("init Alert with conf[{}]".format(self.conf_path))
+    self.conf = yaml.load(open(self.conf_path, 'r'))
+    
+    self.enable_slack = self.conf['slack'].get('enable', True)
+    self.enable_mail = self.conf['mail'].get('enable', True)
+    self.slack = Slacker(self.conf['slack']['token'])
+    self.default_channel = self.conf['slack']['default_channel']
+
+  def send(self, msg, channel = None):
+    if not channel:
+      channel = self.default_channel
+
+    if self.enable_slack:
+      self.send_slack(msg, channel)
+    
+    if self.enable_mail:
+      self.send_mail(msg, channel )
+
+  def send_slack(self, msg, channel):
+    attempt = 0
+    while attempt < self.conf['slack']['retry']:
+      try:
+        self.slack.chat.post_message(channel, msg)
+        logger.info("sending [%s] with channel[%s]" % (msg, channel))
+        return True
+      except Exception, w:
+        logger.error("retry#%s send slack due to [%s]" %(attempt,w))
+        attempt += 1
+    return False 
+
+
+  def send_mail(self, message, channel):
+    subject = "[%s] #%s# %s" % (self.conf['mail'].get('subject_prefix',''), channel, message)
+    username = self.conf['mail']['username']
+    password = self.conf['mail']['password']
+  
+    msg = MIMEMultipart()
+    msg['From'] = username
+    msg['To'] = self.conf['mail']['receiver']
+    msg['Subject'] = subject 
+    msg.attach(MIMEText(message))
     try:
-      ret = send_slack(msg, channel)
-    except Exception, w:
-      print w
-      pass
-
-    return ret
-
-def send_slack(msg, channel="#mooncake"):
-  """Send Message to Slack
-
-  :param msg: 报警内容. 
-  :type msg: str. 
-  :param channel: 接收报警的频道. 
-  :type channel: str. 
-  :returns: bool -- the return code. 
-  :raises: AttributeError, NetworkError
-
-  """ 
-  attempt = 0
-  while attempt < 3:
-    try:
-      slack.chat.post_message(channel, msg)
-      return True
-    except Exception,w:
-      print "retry#%s send slack due to [%s]" %(attempt,w)
-      attempt+=1
-  return False 
+      mailServer = smtplib.SMTP_SSL(self.conf['mail']['smtp_server'],
+                  self.conf['mail']['smtp_port'])
+      mailServer.ehlo()
+      mailServer.login(username, password)
+      mailServer.sendmail(username, self.conf['mail']['receiver'], msg.as_string())
+      mailServer.close()
+    except Exception as e:
+      logger.exception(e)	
 
 if __name__ == "__main__":
-  pass
+  a = Alert()
+  a.send('yuebin')
