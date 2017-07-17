cc4e4b1960ee3030df3e4302ef0874175edf2a9a
ericyue <hi.moonlight@gmail.com>
2017-07-17 20:47:01 +0800

    up

--- mooncake_utils/cython_build.py
+++ mooncake_utils/cython_build.py
@@ -3,16 +3,29 @@ from Cython.Build import cythonize
 import glob
 import os, sys
 from mooncake_utils.cmd import *
-setarg(1, 'build_ext')
-setarg(2, '--inplace')
+
 
 ABSPATH = os.path.dirname(os.path.abspath(sys.argv[0]))
 
 def build(folder = 'lib'):
+  setarg(1, 'build_ext')
+  setarg(2, '--inplace')
   to_build = glob.glob('%s/%s/*.pyx' % (ABSPATH, folder))
   
   for one in to_build:
     setup(ext_modules = cythonize(one))
   
-  run_cmd('rm -rf %s/build' % ABSPATH)
-  run_cmd('rm -rf %s/lib/*.c' % ABSPATH)
+  if len(to_build) >0:
+    run_cmd('rm -rf %s/build' % ABSPATH)
+    run_cmd('rm -rf %s/lib/*.c' % ABSPATH)
+
+def build_pyx(to_build = []):
+  setarg(1, 'build_ext')
+  setarg(2, '-b lib/')
+
+  for one in to_build:
+    setup(ext_modules = cythonize(one))
+ 
+  if len(to_build) >0:
+    run_cmd('rm -rf %s/build' % ABSPATH)
+    run_cmd('rm -rf %s/lib/*.c' % ABSPATH)
