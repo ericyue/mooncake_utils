a8463ebdbd5d7bb00fb59f4b46b192019599b8d1
ericyue <hi.moonlight@gmail.com>
2017-07-22 15:30:14 +0800

    up

---
+++ mooncake_utils/echo.sh
@@ -0,0 +1,493 @@
+#!/usr/bin/env bash
+
+function echo.Black
+{
+    echo -e "\e[30m$@\e[m"
+}
+
+function echo.ULBlack
+{
+    echo -e "\e[4;30m$@\e[m"
+}
+
+function echo.BoldBlack
+{
+    echo -e "\e[1;30m$@\e[m"
+}
+
+function echo.BoldULBlack
+{
+    echo -e "\e[4;1;30m$@\e[m"
+}
+
+function echo.LightBlack
+{
+    echo -e "\e[90m$@\e[m"
+}
+
+function echo.LightULBlack
+{
+    echo -e "\e[4;90m$@\e[m"
+}
+
+function echo.LightBoldBlack
+{
+    echo -e "\e[1;90m$@\e[m"
+}
+
+function echo.LightBoldULBlack
+{
+    echo -e "\e[4;1;90m$@\e[m"
+}
+
+function echo.Red
+{
+    echo -e "\e[31m$@\e[m"
+}
+
+function echo.ULRed
+{
+    echo -e "\e[4;31m$@\e[m"
+}
+
+function echo.BoldRed
+{
+    echo -e "\e[1;31m$@\e[m"
+}
+
+function echo.BoldULRed
+{
+    echo -e "\e[4;1;31m$@\e[m"
+}
+
+function echo.LightRed
+{
+    echo -e "\e[91m$@\e[m"
+}
+
+function echo.LightULRed
+{
+    echo -e "\e[4;91m$@\e[m"
+}
+
+function echo.LightBoldRed
+{
+    echo -e "\e[1;91m$@\e[m"
+}
+
+function echo.LightBoldULRed
+{
+    echo -e "\e[4;1;91m$@\e[m"
+}
+
+function echo.Green
+{
+    echo -e "\e[32m$@\e[m"
+}
+
+function echo.ULGreen
+{
+    echo -e "\e[4;32m$@\e[m"
+}
+
+function echo.BoldGreen
+{
+    echo -e "\e[1;32m$@\e[m"
+}
+
+function echo.BoldULGreen
+{
+    echo -e "\e[4;1;32m$@\e[m"
+}
+
+function echo.LightGreen
+{
+    echo -e "\e[92m$@\e[m"
+}
+
+function echo.LightULGreen
+{
+    echo -e "\e[4;92m$@\e[m"
+}
+
+function echo.LightBoldGreen
+{
+    echo -e "\e[1;92m$@\e[m"
+}
+
+function echo.LightBoldULGreen
+{
+    echo -e "\e[4;1;92m$@\e[m"
+}
+
+function echo.Yellow
+{
+    echo -e "\e[33m$@\e[m"
+}
+
+function echo.ULYellow
+{
+    echo -e "\e[4;33m$@\e[m"
+}
+
+function echo.BoldYellow
+{
+    echo -e "\e[1;33m$@\e[m"
+}
+
+function echo.BoldULYellow
+{
+    echo -e "\e[4;1;33m$@\e[m"
+}
+
+function echo.LightYellow
+{
+    echo -e "\e[93m$@\e[m"
+}
+
+function echo.LightULYellow
+{
+    echo -e "\e[4;93m$@\e[m"
+}
+
+function echo.LightBoldYellow
+{
+    echo -e "\e[1;93m$@\e[m"
+}
+
+function echo.LightBoldULYellow
+{
+    echo -e "\e[4;1;93m$@\e[m"
+}
+
+function echo.Blue
+{
+    echo -e "\e[34m$@\e[m"
+}
+
+function echo.ULBlue
+{
+    echo -e "\e[4;34m$@\e[m"
+}
+
+function echo.BoldBlue
+{
+    echo -e "\e[1;34m$@\e[m"
+}
+
+function echo.BoldULBlue
+{
+    echo -e "\e[4;1;34m$@\e[m"
+}
+
+function echo.LightBlue
+{
+    echo -e "\e[94m$@\e[m"
+}
+
+function echo.LightULBlue
+{
+    echo -e "\e[4;94m$@\e[m"
+}
+
+function echo.LightBoldBlue
+{
+    echo -e "\e[1;94m$@\e[m"
+}
+
+function echo.LightBoldULBlue
+{
+    echo -e "\e[4;1;94m$@\e[m"
+}
+
+function echo.Magenta
+{
+    echo -e "\e[35m$@\e[m"
+}
+
+function echo.ULMagenta
+{
+    echo -e "\e[4;35m$@\e[m"
+}
+
+function echo.BoldMagenta
+{
+    echo -e "\e[1;35m$@\e[m"
+}
+
+function echo.BoldULMagenta
+{
+    echo -e "\e[4;1;35m$@\e[m"
+}
+
+function echo.LightMagenta
+{
+    echo -e "\e[95m$@\e[m"
+}
+
+function echo.LightULMagenta
+{
+    echo -e "\e[4;95m$@\e[m"
+}
+
+function echo.LightBoldMagenta
+{
+    echo -e "\e[1;95m$@\e[m"
+}
+
+function echo.LightBoldULMagenta
+{
+    echo -e "\e[4;1;95m$@\e[m"
+}
+
+function echo.Cyan
+{
+    echo -e "\e[36m$@\e[m"
+}
+
+function echo.ULCyan
+{
+    echo -e "\e[4;36m$@\e[m"
+}
+
+function echo.BoldCyan
+{
+    echo -e "\e[1;36m$@\e[m"
+}
+
+function echo.BoldULCyan
+{
+    echo -e "\e[4;1;36m$@\e[m"
+}
+
+function echo.LightCyan
+{
+    echo -e "\e[96m$@\e[m"
+}
+
+function echo.LightULCyan
+{
+    echo -e "\e[4;96m$@\e[m"
+}
+
+function echo.LightBoldCyan
+{
+    echo -e "\e[1;96m$@\e[m"
+}
+
+function echo.LightBoldULCyan
+{
+    echo -e "\e[4;1;96m$@\e[m"
+}
+
+function echo.White
+{
+    echo -e "\e[37m$@\e[m"
+}
+
+function echo.ULWhite
+{
+    echo -e "\e[4;37m$@\e[m"
+}
+
+function echo.BoldWhite
+{
+    echo -e "\e[1;37m$@\e[m"
+}
+
+function echo.BoldULWhite
+{
+    echo -e "\e[4;1;37m$@\e[m"
+}
+
+function echo.LightWhite
+{
+    echo -e "\e[97m$@\e[m"
+}
+
+function echo.LightULWhite
+{
+    echo -e "\e[4;97m$@\e[m"
+}
+
+function echo.LightBoldWhite
+{
+    echo -e "\e[1;97m$@\e[m"
+}
+
+function echo.LightBoldULWhite
+{
+    echo -e "\e[4;1;97m$@\e[m"
+}
+
+function echo.Purple
+{
+    echo -e "\e[3;38;5;93m$@\e[m"
+}
+
+function echo.ULPurple
+{
+    echo -e "\e[4;3;38;5;93m$@\e[m"
+}
+
+function echo.BoldPurple
+{
+    echo -e "\e[1;3;38;5;93m$@\e[m"
+}
+
+function echo.BoldULPurple
+{
+    echo -e "\e[4;1;3;38;5;93m$@\e[m"
+}
+
+function echo.LightPurple
+{
+    echo -e "\e[9;38;5;93m$@\e[m"
+}
+
+function echo.LightULPurple
+{
+    echo -e "\e[4;9;38;5;93m$@\e[m"
+}
+
+function echo.LightBoldPurple
+{
+    echo -e "\e[1;9;38;5;93m$@\e[m"
+}
+
+function echo.LightBoldULPurple
+{
+    echo -e "\e[4;1;9;38;5;93m$@\e[m"
+}
+
+function echo.Orange
+{
+    echo -e "\e[3;38;5;202m$@\e[m"
+}
+
+function echo.ULOrange
+{
+    echo -e "\e[4;3;38;5;202m$@\e[m"
+}
+
+function echo.BoldOrange
+{
+    echo -e "\e[1;3;38;5;202m$@\e[m"
+}
+
+function echo.BoldULOrange
+{
+    echo -e "\e[4;1;3;38;5;202m$@\e[m"
+}
+
+function echo.LightOrange
+{
+    echo -e "\e[9;38;5;202m$@\e[m"
+}
+
+function echo.LightULOrange
+{
+    echo -e "\e[4;9;38;5;202m$@\e[m"
+}
+
+function echo.LightBoldOrange
+{
+    echo -e "\e[1;9;38;5;202m$@\e[m"
+}
+
+function echo.LightBoldULOrange
+{
+    echo -e "\e[4;1;9;38;5;202m$@\e[m"
+}
+
+function echo.Pink
+{
+    echo -e "\e[3;38;5;206m$@\e[m"
+}
+
+function echo.ULPink
+{
+    echo -e "\e[4;3;38;5;206m$@\e[m"
+}
+
+function echo.BoldPink
+{
+    echo -e "\e[1;3;38;5;206m$@\e[m"
+}
+
+function echo.BoldULPink
+{
+    echo -e "\e[4;1;3;38;5;206m$@\e[m"
+}
+
+function echo.LightPink
+{
+    echo -e "\e[9;38;5;206m$@\e[m"
+}
+
+function echo.LightULPink
+{
+    echo -e "\e[4;9;38;5;206m$@\e[m"
+}
+
+function echo.LightBoldPink
+{
+    echo -e "\e[1;9;38;5;206m$@\e[m"
+}
+
+function echo.LightBoldULPink
+{
+    echo -e "\e[4;1;9;38;5;206m$@\e[m"
+}
+
+function echo.Brown
+{
+    echo -e "\e[3;38;5;52m$@\e[m"
+}
+
+function echo.ULBrown
+{
+    echo -e "\e[4;3;38;5;52m$@\e[m"
+}
+
+function echo.BoldBrown
+{
+    echo -e "\e[1;3;38;5;52m$@\e[m"
+}
+
+function echo.BoldULBrown
+{
+    echo -e "\e[4;1;3;38;5;52m$@\e[m"
+}
+
+function echo.LightBrown
+{
+    echo -e "\e[9;38;5;52m$@\e[m"
+}
+
+function echo.LightULBrown
+{
+    echo -e "\e[4;9;38;5;52m$@\e[m"
+}
+
+function echo.LightBoldBrown
+{
+    echo -e "\e[1;9;38;5;52m$@\e[m"
+}
+
+function echo.LightBoldULBrown
+{
+    echo -e "\e[4;1;9;38;5;52m$@\e[m"
+}
+function  echo.Rainbow
+{
+    if type lolcat > /dev/null 2>&1; then
+        echo "$@" | lolcat
+    else
+        echo "$@"
+    fi
+}
+function  echo.Reset
+{
+    echo "$@" | tr -d '[:cntrl:]' | sed -E "s/\[((;)?[0-9]{1,3}){0,3}m//g"
+}
